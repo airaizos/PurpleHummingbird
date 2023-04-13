33599d51096ff1edc8745db8f338564eea73a00e
adrian <adrian@iraizos.me>
2023-04-13 17:22:11 +0200

    HTTPMethods

---
+++ Sources/PurpleHummingbird/Network/EnumNetwork.swift
@@ -0,0 +1,16 @@
+//
+//  EnumNetwork.swift
+//  
+//
+//  Created by Adrian Iraizos Mendoza on 13/4/23.
+//
+
+import Foundation
+
+public enum HTTPMethods:String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
