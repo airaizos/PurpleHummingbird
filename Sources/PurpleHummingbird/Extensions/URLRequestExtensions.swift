7b348131d0d37e247cb1517a202f3e9156690952
adrian <adrian@iraizos.me>
2023-04-13 19:49:35 +0200

    token

--- Sources/PurpleHummingbird/Extensions/URLRequestExtensions.swift
+++ Sources/PurpleHummingbird/Extensions/URLRequestExtensions.swift
@@ -9,21 +9,34 @@ import Foundation
 
 extension URLRequest {
     
-    static func get(url:URL) -> URLRequest {
+    static func get(url:URL, token:String? = nil) -> URLRequest {
         var request = URLRequest(url: url)
+        
+   
         request.timeoutInterval = 30
         request.httpMethod = HTTPMethods.get.rawValue
         request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        if let token {
+            request.setValue("Beared \(token)", forHTTPHeaderField: "Authorization")
+        }
+        
         return request
     }
     
-    static func post<JSON:Codable>(url:URL,data:JSON, method:HTTPMethods = .post) -> URLRequest {
+    static func post<JSON:Codable>(url:URL,data:JSON, method:HTTPMethods = .post, token:String? = nil) -> URLRequest {
+        
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
         request.timeoutInterval = 30
         request.setValue("application/json; charset=utf8", forHTTPHeaderField: "Content-Type")
         request.setValue("application/json", forHTTPHeaderField: "Accept")
         request.httpBody = try? JSONEncoder().encode(data)
+        
+        if let token {
+            request.setValue("Beared \(token)", forHTTPHeaderField: "Authorization")
+        }
+        
         return request
     }
     
