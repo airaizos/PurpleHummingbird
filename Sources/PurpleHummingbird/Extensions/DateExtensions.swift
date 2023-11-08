369f51d964d00053aca79692ba9ac7bb5e442d03
Adrián Iraizos Mendoza <86532545+airaizos@users.noreply.github.com>
2023-11-08 11:54:49 +0100

    Dates Extension

--- Sources/PurpleHummingbird/Extensions/DateExtensions.swift
+++ Sources/PurpleHummingbird/Extensions/DateExtensions.swift
@@ -9,32 +9,19 @@ import Foundation
 
 public extension Date {
     static let yesterday: Date = {
-        let df = DateFormatter()
-        df.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         if let day = Calendar.current.date(byAdding: .day,value: -1, to: Date.now) {
-            return day
+            return Calendar.current.startOfDay(for: day)
         }
         return Date()
     }()
     
     static let today: Date = {
-           let df = DateFormatter()
-           df.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-            
-           let now = df.string(from: .now)
-        
-           let dateString = now
-           if let date = df.date(from: dateString) {
-               return date
-           } else {
-               return Date()
-           }
+        Calendar.current.startOfDay(for: .now)
     }()
     
     static let tomorrow: Date = {
-        let df = DateFormatter()
-        if let next = Calendar.current.date(byAdding: .day,value: 1, to: Date.now) {
-            return next
+        if let day = Calendar.current.date(byAdding: .day,value: 1, to: Date.now) {
+            return Calendar.current.startOfDay(for: day)
         }
         return Date()
     }()
